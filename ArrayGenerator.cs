a7e22785eff4133af13ba09ba17e431bcf2d28ca
mersedel <xr6033851@gmail.com>
2024-11-19 13:31:13 +0200

    Classes added!

---
+++ ArrayGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal static class ArrayGenerator
+    {
+        internal static int[] GenerateArray(int length, int minNum, int maxNum) 
+        {
+            Random rnd = new Random();
+
+            int[] array = new int[length];
+
+            foreach (int num in array)
+            {
+                array[num] = rnd.Next(minNum, maxNum);
+            }
+
+            return array;
+        }
+    }
+}
---
+++ BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    public static class BubbleSort
+    {
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            bool isSorted = false;
+
+            while (!isSorted)
+            {
+                isSorted = true;
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i].CompareTo(array[i + 1]) > 0)
+                    {
+                        T temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+
+                        isSorted = false;
+                    }
+                }
+            }
+        }
+    }
+}
